c03d00922eadf69435b4a50e53a8c6e27af93332
MohamedRamadan-prog <mohamed.ramadan.lotfy96.com>
2020-05-12 22:42:46 -0500

    add user profile

--- src/main/java/amazon/layer/service/UserService.java
+++ src/main/java/amazon/layer/service/UserService.java
@@ -1,43 +1,16 @@
 package amazon.layer.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
+
 import org.springframework.validation.BindingResult;
 
 import amazon.layer.domainn.User;
 import amazon.layer.dto.UserForm;
-import amazon.layer.mapper.UserMapper;
-import amazon.layer.repository.UserRepository;
 
-@Service
-public class UserService {
+public interface UserService {
 
 	
-	@Autowired
-	UserRepository userRepository;
-
-	public boolean save(UserForm userForm, BindingResult bindingResult)
-	{
-		if (!bindingResult.hasErrors()) { // validation errors		
-    		if (userForm.getPassword().equals(userForm.getPasswordCheck())) { // check password match		
-    				
-    			User user = UserMapper.UserDto(userForm.getName(), userForm.getPassword(), userForm.getUsername(), userForm.getRole());
-		    	if (userRepository.findByEmail(userForm.getUsername()) == null) {
-		    		userRepository.save(user);
-		    	}
-		    	else {
-	    			bindingResult.rejectValue("username", "error.userexists", "Username already exists");    	
-	    			return false;		    		
-		    	}
-    		}
-    		else {
-    			bindingResult.rejectValue("passwordCheck", "error.pwdmatch", "Passwords does not match");    	
-    			return false;
-    		}
-    	}
-    	else {
-    		return false;
-    	}
-    	return true;    	
-    }    
+	public boolean save(UserForm userForm, BindingResult bindingResult);
+	public Optional<User> getUserById(Long id);
+	
 }
