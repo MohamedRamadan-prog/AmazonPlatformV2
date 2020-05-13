5ba21da3c89fbc98d9affceeff6a83d369b7ec5a
Mostafa Mosad <mustafamosad57@gmail.com>
2020-05-13 04:25:07 -0500

    add Review

--- src/main/java/amazon/layer/controller/ReviewController.java
+++ src/main/java/amazon/layer/controller/ReviewController.java
@@ -12,37 +12,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import amazon.layer.domainn.Review;
 import amazon.layer.service.ReviewService;
 
-
 @Controller
 @RequestMapping(value = "/review")
 public class ReviewController {
 
 	@Autowired
 	ReviewService reviewService;
-	
-	
-	@RequestMapping(value = "/getReviws"  , method =  RequestMethod.GET)
-	public String getReviws(Model model)
-	{
+
+	@RequestMapping(value = "/getReviws", method = RequestMethod.GET)
+	public String getReviws(Model model) {
 		Set<Review> reviews = reviewService.getReviews();
-		model.addAttribute("reviews",reviews);
+		model.addAttribute("reviews", reviews);
 		return "adminPindingReview";
 	}
-	
-	
-	@RequestMapping(value = "/enableReview" , method = {RequestMethod.PUT,RequestMethod.GET,RequestMethod.POST} )
-	public String setReviewStatus(@RequestParam("id") Long id)
-	{
+
+	@RequestMapping(value = "/enableReview", method = { RequestMethod.PUT, RequestMethod.GET, RequestMethod.POST })
+	public String setReviewStatus(@RequestParam("id") Long id) {
 		System.out.println("get controller");
 		reviewService.setReviewStatus(id);
 		return "redirect:/review/getReviws";
 	}
-	
-	@RequestMapping(value = "/addReview" , method = {RequestMethod.POST} )
-	public String addReview(@RequestParam("review") String comment, @RequestParam("productId") Long productId)
-	{
-		reviewService.addReview(comment , productId);
-		return "redirect:/review/getReviws";
+
+	@RequestMapping(value = "/addReview", method = { RequestMethod.POST })
+	public String addReview(@RequestParam("review") String comment, @RequestParam("productId") Long productId) {
+		reviewService.addReview(comment, productId);
+		return "redirect:/buyer/ordersHistory";
 	}
 
 }
